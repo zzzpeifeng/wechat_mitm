c851c106cc54a4faf411429652fc1edd1e9f331c
zzzpeifeng <zzzpeifeng@163.com>
2026-01-11 18:20:52 +0800

    增加全流程

--- core/ui/controllers/all_collector.py
+++ core/ui/controllers/all_collector.py
@@ -40,22 +40,22 @@ class AllCollector:
     def get_all_data(self):
         try:
             # # # 发送日志到UI
-            self.log_callback("开始执行吉姆电竞数据收集任务...")
-
-            # 延迟导入自动化处理模块
-            _, _, _, _, _, JiMuProcess = self._import_auto_processes()
-
-            self.process_obj = JiMuProcess()
-            self.process_obj.main_process()
-            time.sleep(5)
-
-            # 检查数据时间戳
-            check_res = self._check_data_timestamp("吉姆电竞")
-
-            # 调用QNDataCollector获取青鸟数据
-            if check_res:
-                self._collect_qn_data('吉姆电竞', check_res)
-
+            # self.log_callback("开始执行吉姆电竞数据收集任务...")
+            #
+            # # 延迟导入自动化处理模块
+            # _, _, _, _, _, JiMuProcess = self._import_auto_processes()
+            #
+            # self.process_obj = JiMuProcess()
+            # self.process_obj.main_process()
+            # time.sleep(5)
+            #
+            # # 检查数据时间戳
+            # check_res = self._check_data_timestamp("吉姆电竞")
+            #
+            # # 调用QNDataCollector获取青鸟数据
+            # if check_res:
+            #     self._collect_qn_data('吉姆电竞', check_res)
+            #
             if self.log_callback:
                 self.log_callback("吉姆电竞数据收集任务完成，开始执行查理熊数据收集任务...")
 
